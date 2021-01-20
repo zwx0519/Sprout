8ea767c2ba74c49135cfed2f594196907389e72b
zwx0519 <15564732161@163.com>
2021-01-20 22:12:58 +0800

    测试

--- app/src/main/java/com/sprout/ui/oasis/home/HomeFragment.kt
+++ app/src/main/java/com/sprout/ui/oasis/home/HomeFragment.kt
@@ -5,28 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.shop.base.BaseFragment
 import com.sprout.R
+import com.sprout.databinding.FragmentHomeBinding
+import com.sprout.viewmodel.oasis.home.HomeViewModel
 
 //TODO 首页页面
-class HomeFragment : Fragment() {
-
-
+class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>
+    (R.layout.fragment_home, HomeViewModel::class.java) {
     //采用伴生对象 companion object==static 只能创建一次
     companion object {
         val instance by lazy { HomeFragment() }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun initView() {
+        mDataBinding.mTabHome
+    }
+
+    override fun initVM() {
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun initData() {
+
     }
+
+    override fun initVariable() {
+
+    }
+
 
 }
---
+++ app/src/main/java/com/sprout/viewmodel/oasis/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.sprout.viewmodel.oasis.home
+
+import com.shop.base.BaseViewModel
+import com.shop.net.Injection
+
+class HomeViewModel : BaseViewModel(Injection.repository){
+}
